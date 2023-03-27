0eecec06656b4b613b18b4f065e4bdb184d0ed12
Coby Kabili <ckabili8@gmail.com>
2023-03-27 15:19:15 -0600

    rows and api

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Banner from '../../components/Nav/Banner';
 import Nav from '../../components/Nav/Nav';
+import Row from '../../components/Nav/Row';
+import requests from '../../Requests';
 import './HomeScreen.css';
 
 function HomeScreen() {
@@ -8,7 +10,39 @@ function HomeScreen() {
     <div className='homeScreen'>
       <Nav />
       <Banner />
-      {/* row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   )
 }
