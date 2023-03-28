4a27baed9483599368e80bab628d10f7e61cbb6a
Coby Kabili <ckabili8@gmail.com>
2023-03-28 09:29:07 -0600

    signup screen

--- src/pages/LoginScreen/LoginScreen.js
+++ src/pages/LoginScreen/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignupScreen from '../SignupScreen/SignupScreen';
 import './LoginScreen.css';
 
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className='loginScreen'>
       <div className='loginScreen__background'>
@@ -10,14 +13,35 @@ function LoginScreen() {
           src="https://i.imgur.com/brdbxe7.png"
           alt="logo"
         />
-        <button className='loginScreen__button'>
+        <button
+          onClick={() => setSignIn(true)}
+          className='loginScreen__button'>
           Sign In
         </button>
         <div className='loginScreen__gradient'/>
       </div>
 
       <div className='loginScreen__body'>
-        
+        {signIn ? (
+          <SignupScreen />
+        ) : (
+        <>
+          <h1>Unlimited movies, TV shows, and more.</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+          <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+          <div className='loginScreen__input'>
+            <form>
+              <input
+                type="email"
+                placeholder='Email Address'
+              />
+              <button
+                onClick={() => setSignIn(true)}
+                className='loginScreen__getStarted'>Get Started</button>
+            </form>
+          </div>
+        </>
+        )}
       </div>
     </div>
   )
