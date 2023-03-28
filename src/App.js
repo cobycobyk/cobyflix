2fb6c30ca3325b4137122ef80d58963e50d10413
Coby Kabili <ckabili8@gmail.com>
2023-03-28 10:06:36 -0600

    user reducer implement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
 import './App.css';
+import { auth } from './firebase';
 import HomeScreen from './pages/HomeScreen/HomeScreen';
 import LoginScreen from './pages/LoginScreen/LoginScreen';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+        
+      }
+    });
+    return unsubscribe;
+  }, [])
+
   return (
     user ? (
     <div className="app">
